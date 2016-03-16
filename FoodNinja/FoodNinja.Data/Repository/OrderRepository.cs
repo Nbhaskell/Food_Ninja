dabf77175b2069d777ed24f7b6c5a2f960729179
Michael Murray <Michael Murray>
2016-03-16 12:58:04 -0700

    Controllers Update - Team and NinjaUser

--- FoodNinja/FoodNinja.Data/Repository/OrderRepository.cs
+++ FoodNinja/FoodNinja.Data/Repository/OrderRepository.cs
@@ -1,11 +1,18 @@
-﻿using System;
+﻿using FoodNinja.Core.Domain;
+using FoodNinja.Core.Repository;
+using FoodNinja.Data.Infrastructure;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
 namespace FoodNinja.Data.Repository
 {
-    public class OrderRepository
+    public class OrderRepository : Repository<Order>, IOrderRepository
     {
+        public OrderRepository(IDatabaseFactory databaseFactory) : base(databaseFactory)
+        {
+
+        }
     }
 }
